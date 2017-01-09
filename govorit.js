fe9ef36a1ebfa4830c9dd82baf5ba65c8b757836
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2017-01-09 12:03:37 +0100

    Wrap the word fetching mechanism in a function.

--- govorit.js
+++ govorit.js
@@ -1,24 +1,29 @@
-// TODO: This should be wrapped in a function.
-$.ajax({
-    url: 'https://ru.wiktionary.org/w/api.php',
-    jsonpCallback: '__wmcb__',
-    dataType: 'jsonp',
-    data: {
-        action: 'query',
-        list: 'random',
-        rnlimit: '10',
+function fetchWords(queue) {
+    $.ajax({
+        url: 'https://ru.wiktionary.org/w/api.php',
+        jsonpCallback: '__wmcb__',
+        dataType: 'jsonp',
+        data: {
+            action: 'query',
+            list: 'random',
+            rnlimit: '10',
 
-        format: 'json',
-        callback: '__wmcb__',
-    },
-    success: function (response) {
-        let words = response.query.random
-            .map((entry) => entry.title)
-            .filter((word) => word.match(/^[\u0430-\u044f]+$/i))
-            .filter((word) => word.length >= 3);
-        // TODO: Pass results to the callback function.
-        // TODO: Repeat process if not enough words.
-        console.log(words);
-    },
-    // TODO: Add support for handling failures.
-});
+            format: 'json',
+            callback: '__wmcb__',
+        },
+        success: function (response) {
+            let words = response.query.random
+                .map((entry) => entry.title)
+                .filter((word) => word.match(/^[\u0430-\u044f]+$/i))
+                .filter((word) => word.length >= 3);
+            queue.push(...words);
+            if (queue.length < 10) {
+                fetchWords(queue);
+            }
+        },
+        // TODO: Add support for handling failures.
+    });
+}
+
+let queue = [];
+fetchWords(queue);
